95e48f0c5193ad2e31ec44418bc3c6f2c6d0901f
Tobi <developer@onmyown.at>
2016-08-18 17:28:03 +0200

    midif: load only min js to server

--- meta/footer.php
+++ meta/footer.php
@@ -10,4 +10,12 @@
 	<!-- TODO: load with requireJS -->
 	<script type="text/javascript" src="js/koObjects/core_db_data.js"></script>
 	<script type="text/javascript" src="js/koObjects/user_data.js"></script>
+<?php
+
+if(file_exists("js/script.js")){
+	echo "exists";
+}else{
+	echo "not there";
+}
+ ?>
 </footer>
