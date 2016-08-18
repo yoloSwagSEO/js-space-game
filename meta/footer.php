d9dc1a917c5ecde3b202ad2107c0ffd992f70386
Tobi <developer@onmyown.at>
2016-08-18 17:31:35 +0200

    modif: footer.php

--- meta/footer.php
+++ meta/footer.php
@@ -1,21 +1,15 @@
 <footer>
 	<!-- StyleSheets -->
 	<link href="css/dist/main.css" rel="stylesheet" type="text/css">
-
-	<!-- MINIFIED -->
-	<!-- <script type="text/javascript" src="js/dist/composite.all.min.js"></script> -->
+<?php if(file_exists("js/script.js")){ ?>
 	<!-- main.js -->
 	<script type="text/javascript" src="js/script.js"></script>
 	<!-- Objects.js -->
 	<!-- TODO: load with requireJS -->
 	<script type="text/javascript" src="js/koObjects/core_db_data.js"></script>
 	<script type="text/javascript" src="js/koObjects/user_data.js"></script>
-<?php
-
-if(file_exists("js/script.js")){
-	echo "exists";
-}else{
-	echo "not there";
-}
- ?>
+<?php }else{ ?>
+	<!-- MINIFIED -->
+	<script type="text/javascript" src="js/dist/composite.all.min.js"></script>
+<?php } ?>
 </footer>
