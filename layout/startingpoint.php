0e6821bdcb1c0a36c5c700963c1009412d4b0833
Tobi <developer@onmyown.at>
2016-08-20 18:51:50 +0200

    add: startingpoint.php

---
+++ layout/startingpoint.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Solarsystem</title>
+		<link rel="stylesheet" href="solarsystemStyles.css" media="screen">
+	</head>
+	<body>
+		<h2>Pick your Solarsystem...</h2>
+		<p>ölkjflkjashdfökjhadfö</p>
+		<p>sdfjölkasdjfölkadsjfölakdjsf</p>
+		<p>adsjfölkasjdfökljasdf</p>
+		<div class="solarsystem_box system1">
+			<span class="left">
+				<h3>XP76Z</h3>
+				<p>ölkjflkjashdfökjhadfö</p>
+				<p>sdfjölkasdjfölkadsjfölakdjsf</p>
+				<p>adsjfölkasjdfökljasdf</p>
+			</span>
+			<span class="right">
+				<div class="buff buff1">
+					buff 1
+				</div>
+				<div class="buff buff2">
+					buff 2
+				</div>
+				<div class="buff buff3">
+					buff 3
+				</div>
+			</span>
+		</div>
+	</body>
+</html>
