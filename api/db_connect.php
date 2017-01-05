d849b717f2a22daebd59530b99a94c89087c3be6
Tobi <developer@onmyown.at>
2017-01-05 20:53:23 +0100

    live db

--- api/db_connect.php
+++ api/db_connect.php
@@ -1,18 +1,18 @@
 <?php
 
 	// live
-	// $hostname  = "dd1816.kasserver.com";
-	// $hostport  = "";
-	// $database  = "d02260f9";
-	// $username  = "d02260f9";
-	// $password  = "JRv-Qp4-M3h-UxJ";
+	$hostname  = "dd1816.kasserver.com";
+	$hostport  = "";
+	$database  = "d02260f9";
+	$username  = "d02260f9";
+	$password  = "JRv-Qp4-M3h-UxJ";
 
 	// localhost
-	$hostname  = "localhost";
-	$hostport  = "8889";
-	$database  = "js-space-game";
-	$username  = "root";
-	$password  = "root";
+	// $hostname  = "localhost";
+	// $hostport  = "8889";
+	// $database  = "js-space-game";
+	// $username  = "root";
+	// $password  = "root";
 
 	//connection to the database
 	$dbhandle = mysqli_connect($hostname, $username, $password, $database, $hostport) or die("Unable to connect to mysqli");
@@ -20,18 +20,18 @@
 	//$selected = mysqli_select_db($database,$dbhandle) or die("Could not select examples");
 
 	//live
-	// define('DBHOST','dd1816.kasserver.com');
-	// define('DBUSER','d02260f9');
-	// define('DBPASS','JRv-Qp4-M3h-UxJ');
-	// define('DBNAME','d02260f9');
-	// define('DBPORT', '');
+	define('DBHOST','dd1816.kasserver.com');
+	define('DBUSER','d02260f9');
+	define('DBPASS','JRv-Qp4-M3h-UxJ');
+	define('DBNAME','d02260f9');
+	define('DBPORT', '');
 
 	// localhost
-	define('DBHOST', 'localhost');
-	define('DBUSER', 'root');
-	define('DBPASS', 'root');
-	define('DBNAME', 'js-space-game');
-	define('DBPORT', '8889');
+	// define('DBHOST', 'localhost');
+	// define('DBUSER', 'root');
+	// define('DBPASS', 'root');
+	// define('DBNAME', 'js-space-game');
+	// define('DBPORT', '8889');
 
 	//application address
 	define('DIR','http://onmyown.at/jsgame/');
